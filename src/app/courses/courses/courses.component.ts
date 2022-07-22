032a504e44fb4b97e1a6cc5b104ed51196efd100
Igor de Souza Bezerra <igorbezerra01@outlook.com>
2022-07-22 12:28:24 -0300

    feat: material table to list courses

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Courses } from '../models/courses';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.scss']
+  styleUrls: ['./courses.component.scss'],
 })
 export class CoursesComponent implements OnInit {
+  courses: Courses[];
+  displayedColumns: string[] = ['name', 'category']
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.courses = [
+      {
+        _id: '1', name: 'angular', category: `frontend`
+      }
+    ]
   }
 
+  ngOnInit(): void {}
 }
