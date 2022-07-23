fa1a6b2b1e12fc61a32b5af68dfcd72cb3679bb7
Igor de Souza Bezerra <igorbezerra01@outlook.com>
2022-07-23 18:20:31 -0300

    initial commit spring

---
+++ crud-courses-spring/src/main/java/dev/igor/crudcoursesspring/CrudCoursesSpringApplication.java
@@ -0,0 +1,13 @@
+package dev.igor.crudcoursesspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudCoursesSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudCoursesSpringApplication.class, args);
+	}
+
+}
---
+++ crud-courses-spring/src/test/java/dev/igor/crudcoursesspring/CrudCoursesSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.igor.crudcoursesspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudCoursesSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
